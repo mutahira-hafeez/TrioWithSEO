2a880ebe127e861fa6b1e3422e24c9103962faea
Mutahira Hafeez <mutahira.hafeez@qbatch.com>
2025-06-11 15:47:20 +0500

    db connected

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,6 +32,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // chapter 7 => Fetching Invoices
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
